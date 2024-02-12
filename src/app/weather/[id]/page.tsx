b4290ca22bb163ded25cbc6f6a0e9d1969865b0d
Rodrigo Crespo <cresporodrigomanuel@gmail.com>
2024-02-12 20:09:25 -0300

    Remove weather page

--- src/app/weather/[id]/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import { useParams } from 'next/navigation'
-
-export default function Page() {
-    const params = useParams<{ id: string }>()
-    return <p>Post: {params.id}</p>
-}
