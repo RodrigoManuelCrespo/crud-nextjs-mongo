1ce299706b33cb3309c5d8da6e745b43c5811c71
Rodrigo Crespo <cresporodrigomanuel@gmail.com>
2024-02-25 10:48:08 -0300

    Update next config cors

--- next.config.js
+++ next.config.js
@@ -2,25 +2,24 @@ const nextConfig = {
     async headers() {
         return [
             {
-                source: "/api/tasks",
+                // matching all API routes
+                source: "/api/(.*)",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "https://crud-nextjs-mongo-gm7rfe9gd-rodrigos-projects-0d6b391d.vercel.app/api/tasks" }, // Replace with actual origin
-                    { key: "Access-Control-Allow-Methods", value: "GET, POST" },
-                    { key: "Access-Control-Allow-Headers", value: "Content-Type" },
-                ]
+                    { key: "Access-Control-Allow-Origin", value: "*" },
+                    {
+                        key: "Access-Control-Allow-Methods",
+                        value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+                    },
+                    {
+                        key: "Access-Control-Allow-Headers",
+                        value:
+                            "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+                    },
+                ],
             },
-            {
-                source: "/api/weather",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "https://crud-nextjs-mongo-gm7rfe9gd-rodrigos-projects-0d6b391d.vercel.app/api/weather" }, // Replace with actual origin
-                    { key: "Access-Control-Allow-Methods", value: "GET" },
-                    { key: "Access-Control-Allow-Headers", value: "Content-Type" },
-                ]
-            }
-        ];
-    }
+        ]
+    },
 };
 
 module.exports = nextConfig;
