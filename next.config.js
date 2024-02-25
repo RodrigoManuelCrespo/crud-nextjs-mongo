62dacabdab3baee463d1632eca185af324dccbcd
Rodrigo Crespo <cresporodrigomanuel@gmail.com>
2024-02-25 10:36:44 -0300

    Fix cors header

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
                 source: "/api/tasks",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "https://crud-nextjs-mongo-gm7rfe9gd-rodrigos-projects-0d6b391d.vercel.app/" }, // Replace with actual origin
+                    { key: "Access-Control-Allow-Origin", value: "https://crud-nextjs-mongo-gm7rfe9gd-rodrigos-projects-0d6b391d.vercel.app/api/tasks" }, // Replace with actual origin
                     { key: "Access-Control-Allow-Methods", value: "GET, POST" },
                     { key: "Access-Control-Allow-Headers", value: "Content-Type" },
                 ]
@@ -14,7 +14,7 @@ const nextConfig = {
                 source: "/api/weather",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "https://crud-nextjs-mongo-gm7rfe9gd-rodrigos-projects-0d6b391d.vercel.app/" }, // Replace with actual origin
+                    { key: "Access-Control-Allow-Origin", value: "https://crud-nextjs-mongo-gm7rfe9gd-rodrigos-projects-0d6b391d.vercel.app/api/weather" }, // Replace with actual origin
                     { key: "Access-Control-Allow-Methods", value: "GET" },
                     { key: "Access-Control-Allow-Headers", value: "Content-Type" },
                 ]
