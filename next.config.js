3efe2ede7550d05554c6f39d1fcc82606a79787c
Rodrigo Crespo <cresporodrigomanuel@gmail.com>
2024-02-25 10:21:43 -0300

    Fix next config cors

--- next.config.js
+++ next.config.js
@@ -1,19 +1,26 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
     async headers() {
         return [
             {
-                // matching all API routes
-                source: "/api/:path*",
+                source: "/api/tasks",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                    { key: "Access-Control-Allow-Origin", value: "https://crud-nextjs-mongo-gm7rfe9gd-rodrigos-projects-0d6b391d.vercel.app/" }, // Replace with actual origin
+                    { key: "Access-Control-Allow-Methods", value: "GET, POST" },
+                    { key: "Access-Control-Allow-Headers", value: "Content-Type" },
+                ]
+            },
+            {
+                source: "/api/weather",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "https://crud-nextjs-mongo-gm7rfe9gd-rodrigos-projects-0d6b391d.vercel.app/" }, // Replace with actual origin
+                    { key: "Access-Control-Allow-Methods", value: "GET" },
+                    { key: "Access-Control-Allow-Headers", value: "Content-Type" },
                 ]
             }
-        ]
+        ];
     }
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
